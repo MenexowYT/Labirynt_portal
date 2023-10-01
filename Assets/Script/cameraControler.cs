cb07cc4845e4fd093ad9976e09f40a41a4bf728f
MenexowYT <145260354+MenexowYT@users.noreply.github.com>
2023-10-01 13:21:17 +0200

    Gravity, gamePaused, gameRewarsed

---
+++ Assets/Script/cameraControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControler : MonoBehaviour
+{
+    [SerializeField]
+    float mouseSensitivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; // xRotation = xRotation - mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Script/playerControler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 12f;
+	CharacterController characterController;
+	float gravity = -7.81f;
+	
+	public Transform groundChecker;
+	public LayerMask groundMask;
+
+	private void Start()
+	{
+		characterController = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		PlayerMove();
+	}
+
+	private void PlayerMove()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		Vector3 move = transform.right * x + transform.forward * z;
+		CheckGroundType();
+		characterController.Move(move * speed * Time.deltaTime);
+		ApplyGravity();
+	}
+
+	void ApplyGravity()
+	{
+		characterController.Move(Vector3.up * gravity * Time.deltaTime);
+	}
+
+	void CheckGroundType()
+	{
+		RaycastHit hitResult;
+		bool hit = Physics.Raycast(
+			groundChecker.position,
+			transform.TransformDirection(Vector3.down),
+			out hitResult,
+			0.4f,
+			groundMask);
+
+		if (hit)
+		{
+			string terrainType = hitResult.collider.gameObject.tag;
+			switch(terrainType)
+			{
+				case "Slow":
+					speed = 3f;
+					break;
+				case "Fast":
+					speed = 20f;
+					break;
+				default:
+					speed = 10f;
+					break;
+			}
+		}
+	}
+}
